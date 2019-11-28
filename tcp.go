8b0af03e6441f0e6d4975819d9bebadc5ace5719
Dariush Abbasi <dariushem@yahoo.com>
2019-11-28 11:56:58 +0330

    add tls port check

--- tcp.go
+++ tcp.go
@@ -2,6 +2,7 @@ package libtower
 
 import (
 	"context"
+	"crypto/tls"
 	"net"
 	"net/url"
 	"time"
@@ -11,6 +12,9 @@ import (
 type TCP struct {
 	URL     *url.URL
 	Timeout time.Duration
+
+	CertFile       string
+	PrivateKeyFile string
 
 	Start    time.Time
 	End      time.Time
@@ -31,4 +35,29 @@ func (tr *TCP) TCPPortCheck(ctx context.Context) (bool, error) {
 		return true, nil
 	}
 	return false, nil
+}
+
+// TLSPortCheck check if a scured tcp port is open
+func (tr *TCP) TLSPortCheck(ctx context.Context) (bool, error) {
+	cert, err := tls.LoadX509KeyPair(tr.CertFile, tr.PrivateKeyFile)
+	if err != nil {
+		return false, err
+	}
+	config := tls.Config{Certificates: []tls.Certificate{cert}, InsecureSkipVerify: true}
+
+	tr.Start = time.Now()
+	conn, err := net.DialTimeout("tcp", tr.URL.Host, tr.Timeout)
+	if err != nil {
+		return false, err
+	}
+
+	tlsConn := tls.Client(conn, &config)
+	err = tlsConn.Handshake()
+	if err != nil {
+		return false, err
+	}
+	tr.End = time.Now()
+	tr.Duration = tr.End.Sub(tr.Start)
+
+	return true, nil
 }
--- tcp_test.go
+++ tcp_test.go
@@ -9,12 +9,27 @@ import (
 
 func TestTCPPortCheck(t *testing.T) {
 	ctx := context.TODO()
+
 	URL, err := url.Parse("tcp://google.com:80")
 	if err != nil {
 		t.Errorf("test failed %v", err)
 	}
 	tr := TCP{URL: URL, Timeout: time.Second * 2}
 	_, err = tr.TCPPortCheck(ctx)
+	if err != nil {
+		t.Errorf("test failed %v", err)
+	}
+}
+
+func TestTLSPortCheck(t *testing.T) {
+	ctx := context.TODO()
+
+	URL, err := url.Parse("tcp://google.com:443")
+	if err != nil {
+		t.Errorf("test failed %v", err)
+	}
+	tr := TCP{URL: URL, Timeout: time.Second * 2, CertFile: "data/certs/client.pem", PrivateKeyFile: "data/certs/client.key"}
+	_, err = tr.TLSPortCheck(ctx)
 	if err != nil {
 		t.Errorf("test failed %v", err)
 	}
