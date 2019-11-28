d304491afa8b6aee9339a12f87eebbd8f36331e3
Dariush Abbasi <dariushem@yahoo.com>
2019-11-28 11:31:49 +0330

    add key pairs for tls checks

---
+++ data/mkcrt.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+openssl req -new -nodes -x509 -out certs/server.pem -keyout certs/server.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Random Company/OU=IT/CN=$1/emailAddress=$2"
+openssl req -new -nodes -x509 -out certs/client.pem -keyout certs/client.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Random Company/OU=IT/CN=$1/emailAddress=$2"
